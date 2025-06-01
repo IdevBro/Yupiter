1a6bad23a2d3b6cc6aafc8f31e68ff26f69e9f0c
IDevBro <mirsharipov.m98@gmail.com>
2025-06-01 15:57:07 +0500

    correctly groups

--- components/father/groups/index.tsx
+++ components/father/groups/index.tsx
@@ -2,11 +2,11 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 
 export default function Group() {
   return (
-    <ScrollArea className="h-[100vh] w-30 rounded-md border border-gray-300">
+    <ScrollArea className="h-[100vh] w-[150px] rounded-md border border-gray-300">
       <div className="p-4">
         {Array.from({ length: 100 }).map((_, i) => (
           <div key={i} className="text-sm py-2 border-b">
-            nF {i + 1}
+            <p className="text-[18px] font-bold"> nF {i + 1}</p>
           </div>
         ))}
       </div>
