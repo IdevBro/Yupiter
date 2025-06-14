4ffb944b29b0c68d1df63d16f4f5feb96035baea
suyunovdev <153995037+suyunovdev@users.noreply.github.com>
2025-06-14 12:14:04 +0500

    color add

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@
 export default function Home() {
   return (
     <div className="">
-      <main className="">
-        <h1 className=" p-4 rounded-lg">Salom!</h1>
+      <main className="bg-amber-300">
+        <h1 className=" p-4 rounded-lg ">Salom!</h1>
       </main>
     </div>
   );
